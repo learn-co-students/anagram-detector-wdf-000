b130d5137776db56fd541e5dcd317b95b3561099
blessedsoy <unmigong@gmail.com>
2016-06-24 16:27:12 -0400

    OO Anagram Detector

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    result = []
+    array.each do |word| 
+    result << word if word.split('').sort == self.word.split('').sort 
+    end
+    result
+  end
+
+end
