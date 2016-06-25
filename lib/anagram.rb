de929c542ea6b403156011081ef3e1ef279e1e77
Gordon Gao <gordonsdev@gmail.com>
2016-06-25 17:08:07 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(arr)
+    sorted_word = sort(@word)
+    arr.select { |word| sorted_word == sort(word) } || []
+  end
+
+  private
+
+    def sort(word)  # => sorted word
+      word.chars.sort.join("")
+    end
+
+end
