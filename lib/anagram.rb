0bcaf51948ff36ae408894c3d63afada205851fc
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-06-28 17:19:23 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+attr_accessor :word
+
+def initialize(word)
+  @word = word
+end
+
+def match(array)
+  array.select{|test_word| (test_word.split("").sort <=> @word.split("").sort) == 0}
+end
+
+
+end
