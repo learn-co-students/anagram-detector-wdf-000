1af509d178fd1ba384b1c4f1c61f892460fe6ea2
ckshei <cks856@gmail.com>
2016-06-28 13:36:29 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.select do |w|
+      w.chars.sort(&:casecmp).join == @word.chars.sort(&:casecmp).join
+    end
+  end
+
+end
