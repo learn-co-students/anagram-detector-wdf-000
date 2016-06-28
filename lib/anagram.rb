97387f32c3e0705ed325464d4278c00ddf91608b
ozPop <ozziepopovas@gmail.com>
2016-06-28 16:15:29 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
+require 'pry'
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(arr)
+    arr.select { |arr_word| word.split("").sort == arr_word.split("").sort }
+  end
+end
