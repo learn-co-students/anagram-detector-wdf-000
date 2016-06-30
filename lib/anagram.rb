8d0c709d1763369b027b7445834c0a8f7e1eb43e
fidelscodes <fidelscodes@gmail.com>
2016-06-30 15:16:44 -0400

    Update with solution

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(possible_anagrams)
+    # anagrams = []
+    # possible_anagrams.each do |element|
+    #   anagrams << element if element.split("").sort == self.word.split("").sort
+    # end
+    # anagrams
+
+    # using .map chained with .compact avoids code smell!
+    possible_anagrams.map do |element|
+      element if element.split("").sort == word.split("").sort
+    end.compact
+  end
+end
