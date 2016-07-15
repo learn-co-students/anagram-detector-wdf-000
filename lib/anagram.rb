3eb046dba75e36c5bb04584ec62b2381bec931ba
Derick Quartey <niikwartey1@gmail.com>
2016-07-15 13:37:20 -0400

    finish anagram detector lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(arr)
+    arr.select do |item|
+      word_sort(item) == word_sort(@word)
+    end
+  end
+
+  def word_sort(word)
+    word.split("").sort.join
+  end
+end
