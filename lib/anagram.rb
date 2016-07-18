1d18df840f3a2ac2f166e0fd87bc138a25f11181
Esmery Corniel <esmerycornielle@gmail.com>
2016-07-18 11:45:34 -0400

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,40 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    self.word = word
+  end
+
+  def match(array_anagram)
+
+    #this algorithm is not complete
+    # c =0
+    # result = []
+    #
+    # array_anagram.collect do |anagram|
+    #   if anagram.length == self.word.length
+    #     self.word.split("").collect do |letter|
+    #       anagram.split("")
+    #       while c<anagram.length-1
+    #         if letter == anagram[i]
+    #           result[i] = anagram[i]
+    #         end
+    #       end
+    #     end
+    #   end
+
+
+    # this is the efficient way of doing it
+    result = []
+    array_anagram.collect do |anagram|
+      if anagram.split("").sort == self.word.split("").sort
+        result = anagram
+        result
+      end
+    end.compact.flatten
+  end
+
+
+end
