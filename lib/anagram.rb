6e88e4800990aa13806437e4959be17c0622d181
parm530 <parmsahadeo530@gmail.com>
2016-07-20 17:55:15 -0400

    Created Anagram class with #match method

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(string)
+    @word = string
+  end
+
+  def match(array)
+    w_array = @word.split("").sort
+    temp_array = []
+
+    array.each do |element|
+      e_array = element.split("").sort
+      if(e_array == w_array)
+        temp_array << element
+      end
+    end
+    temp_array
+  end
+
+end
