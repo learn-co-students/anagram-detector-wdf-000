978c9621b4751bdd784aac9e3c21c6ee77f40e68
Bibi Khadiza <limakhadiza@yahoo.com>
2016-07-21 17:42:19 -0400

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,31 @@
+require 'pry'
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    empty_array = []
+      array.each do |element|
+        if element.split(//).sort == @word.split(//).sort
+          empty_array << element
+          empty_array
+        end
+      end
+        empty_array
+  end
+
+end
+
+
+
+
+
+
+
+
+
+
 # Your code goes here!
