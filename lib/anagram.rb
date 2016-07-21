810f180272a81ce7719dd1a8bd3093cad22f9e75
Kelene Chung <simoy93@gmail.com>
2016-07-21 17:41:51 -0400

    Anagram Detector done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
+require 'pry'
 # Your code goes here!
+class Anagram
+
+attr_accessor :words
+
+
+	def initialize(words)
+		@words = words
+	end
+
+	def match(string)
+		arr = []
+		string.each do |var|
+			if var.chars.sort == @words.chars.sort 
+				arr << var
+				arr
+			end
+		end
+		arr
+	end
+
+
+
+
+
+
+
+end
