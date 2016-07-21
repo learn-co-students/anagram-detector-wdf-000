38f9801cd70dcf7aed075dbfb45d1a0763e1b770
Bibi Khadiza <limakhadiza@yahoo.com>
2016-07-21 18:00:03 -0400

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,24 +6,21 @@ class Anagram
     @word = word
   end
 
-  def match(array)                   #create a instance method with an argument of an array
-    empty_array = []                #create the empty array
-      array.each do |element|       #iterate on the array given.
-        if element.split(//).sort == @word.split(//).sort #slpit the element (it becomes an array)
-          empty_array << element        #the(//) creates charaters and sort sorts the characters
-        end                #in apha order. we compare. shovel the truthy elements into
-      end                            #the empty array. if the element is not a truthy
-      empty_array                     #do not shovel. return array.
+  def match(array)                   
+    empty_array = []
+      array.each do |element|
+        if element.split(//).sort == @word.split(//).sort
+          empty_array << element
+        end
+      end
+      empty_array
   end
 end
 
-
-
-
-
-
-
-
-
-
-# Your code goes here!
+ #create a instance method with an argument of an array
+#create the empty array
+#slpit the element (it becomes an array)
+#the(//) creates charaters and sort sorts the characters
+ #in apha order. we compare. shovel the truthy elements into
+ #the empty array. if the element is not a truthy
+  #do not shovel. return array.
